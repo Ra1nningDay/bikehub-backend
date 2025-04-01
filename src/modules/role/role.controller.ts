73ebf72f391ca611dd053f0bfb2f4d44e53dece0
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-04-02 01:37:34 +0700

    built: api endpoint to create role

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,12 +1,23 @@
-import { Get } from '@nestjs/common';
+import { Body, Get, Post, Param } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import { RoleService } from './role.service';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Controller('role')
 export class RoleController {
   constructor(private roleService: RoleService) {}
   @Get('/')
-  findAllUser() {
+  findAllRole() {
     return this.roleService.getAllRole();
+  }
+
+  @Get('/:id')
+  findRole(@Param('id') id: string) {
+    return this.roleService.getById(+id);
+  }
+
+  @Post('/')
+  createRole(@Body() createRoleDto: CreateRoleDto) {
+    return this.roleService.createRole(createRoleDto);
   }
 }
--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { roles } from '@prisma/client';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Injectable()
 export class RoleService {
@@ -10,6 +11,14 @@ export class RoleService {
   }
 
   async getById(id: number): Promise<roles | null> {
-    return await this.prisma.roles.findUnique({ where: { id } });
+    return await this.prisma.roles.findUniqueOrThrow({ where: { id } });
+  }
+
+  async createRole(createRoleDto: CreateRoleDto): Promise<roles> {
+    return await this.prisma.roles.create({
+      data: {
+        ...createRoleDto,
+      },
+    });
   }
 }
