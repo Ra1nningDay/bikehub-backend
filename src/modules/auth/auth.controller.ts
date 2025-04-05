36f7de18cc39df6d4b3ff790a97556ee2e3ef1c8
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-04-06 02:18:48 +0700

    test

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -13,11 +13,15 @@ import {
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {}
 
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
@@ -44,8 +48,13 @@ export class AuthController {
     await this.authService.insertUser(user);
 
     const userData = encodeURIComponent(JSON.stringify(user));
+    const frontendUrl = this.configService.get<string>('URL_FRONTEND'); // อ่าน URL_FRONTEND
+
+    if (!frontendUrl) {
+      throw new Error('URL_FRONTEND is not defined');
+    }
     res.redirect(
-      `${process.env.URL_FRONTEND}/auth/callback?token=${token}&user=${userData}`,
+      `${frontendUrl}/auth/callback?token=${token}&user=${userData}`,
     );
   }
 }
